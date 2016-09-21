1674103a83498c41594a6698482d65a8aae42dc4
Cosimo89 <cosimo.livi@gmail.com>
2016-09-21 14:49:52 +0200

    Finite element folder

---
+++ code/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from mesh import *
+from basis_func import *
+from assemble import *
+
+from viewer import *
+
+def clear_rows(A,b_nodes):
+    """ code to clera rows """
+# d=np.diag(A)
+# A[b_nodes,:]=0
+
+
+if __name__ == "__main__":
