9caa8c55b337adc226c4c2706f7320fbecb4f0c9
Simone Martini <simomartini@MacBook-di-Simone-2.local>
2016-09-22 12:11:42 +0200

    second step

--- code/mesh.py
+++ code/mesh.py
@@ -2,21 +2,36 @@ import numpy as np
 
 def read_msh(filenamne):
 
+#Initialisation
     x = np.array([])
     y = np.array([])
-
+    b_nodes= np.array([])
+    topo= np.array([])
+    nodes=np.array([])
 
     """ read mesh code """
     f = open(filenamne, 'r')
-#Salta le righe con il dollaro	
+
+#Creation of the vector and matricies	
     for line in f:
         if line[0]=='$':
             print("this is useless")
         else:
+            l =  map(float,line.split())
+            if len(l) == 4:
+                x = np.append(x,l[1])
+                y = np.append(y,l[2])
+            if len(l) == 7:
+                #boundary
+                b_nodes = np.append(b_nodes,l[6])
+                b_nodes = map(int,b_nodes)
+            if len(l) == 8:
+                # domain
+                topo = np.append(topo,l[5],l[6],l[7])
             print(line)
-#
+
 	
-#controllo elementi 
+#Check the elements
     r_id = 0
     for row in topo:
         ck =      (x[row[1]]-x[row[0]])*(y[row[2]]-y[row[0]])
@@ -27,3 +42,5 @@ def read_msh(filenamne):
 
     print r_id
     return topo , x , y , nodes , b_nodes
+
+
