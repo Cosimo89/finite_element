9c71b23402d2b4c2b0173dcacac66997ad986f6c
Simone Martini <simomartini@MacBook-di-Simone-2.local>
2016-09-22 15:16:27 +0200

    main

--- code/main.py
+++ code/main.py
@@ -6,10 +6,16 @@ from assemble import *
 
 from viewer import *
 
-def clear_rows(A,b_nodes):
+def clear_rows(A,b_nodes,f):
     """ code to clera rows """
-# d=np.diag(A)
-# A[b_nodes,:]=0
 
+    d=np.diag(A)
+    B=np.diag(d)
+    A=A-B
+    A[b_nodes,:]=0
+    A=A+B
+    f[b_nodes]=0
+    return A,f
 
 if __name__ == "__main__":
+    read_msh(/mesh/square.msh)
